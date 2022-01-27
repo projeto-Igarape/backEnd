d99957935b9a8807edca3972a74f3e16610d2cf2
Karol  Soares <karolina.soares@sumup.com>
2022-01-27 12:12:44 -0300

    Task 6

--- Spring - Tasks/src/main/java/br/com/generation/projetoigarape/model/Categoria.java
+++ Spring - Tasks/src/main/java/br/com/generation/projetoigarape/model/Categoria.java
@@ -1,12 +1,18 @@
 package br.com.generation.projetoigarape.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name="tb_categoria")
@@ -23,9 +29,13 @@ private String descricao;
 @Size(min=2,max=50,message="A seção deve conter entre 2 e 50 caracteres")
 private String secao;
 
+@OneToMany (mappedBy="categoria",cascade = CascadeType.ALL)
+@JsonIgnoreProperties("categoria")
+private List<Produto> produto;
+
 public Long getId() {
 	return id;
-} 
+}
 
 public void setId(Long id) {
 	this.id = id;
@@ -45,5 +55,15 @@ public String getSecao() {
 
 public void setSecao(String secao) {
 	this.secao = secao;
-	}
+}
+
+public List<Produto> getProduto() {
+	return produto;
+}
+
+public void setProduto(List<Produto> produto) {
+	this.produto = produto;
+}
+
+
 }
